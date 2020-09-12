35eb98350b6e7e9f4108e8a29b8aaf956e920981
Ariane Oliveira <oliveiraariane2@gmail.com>
2020-09-12 12:31:06 -0300

    Commit inicial

---
+++ src/test/java/br/com/nubank/client/ClientApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.nubank.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
